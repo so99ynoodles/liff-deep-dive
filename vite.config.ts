b2175918dbd0af2d7fac404a6187ad8fcb11641c
so99ynoodles <so99ynoodles@gmail.com>
2022-04-21 12:10:35 +0900

    Fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,21 @@
-import { defineConfig } from 'vite';
-import fs from 'fs';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import fs from "fs";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    https: {
-      key: fs.readFileSync('./localhost-key.pem'),
-      cert: fs.readFileSync('./localhost.pem'),
+export default defineConfig(({ mode }) => {
+  if (mode !== "production") {
+    return {
+      plugins: [react()],
+      server: {
+        https: {
+          key: fs.readFileSync("./localhost-key.pem"),
+          cert: fs.readFileSync("./localhost.pem")
+        }
+      }
     }
   }
+  return {
+    plugins: [react()],
+  };
 });
