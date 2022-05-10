816cea4eb55c235de9a90e9fc9741b8a8e74cdec
cola119 <kohei.ueno119@gmail.com>
2022-05-10 13:34:20 +0900

    install liff-inspector

--- src/hooks/useLiff.ts
+++ src/hooks/useLiff.ts
@@ -1,10 +1,12 @@
 import liff from "@line/liff";
+import LIFFInspectorPlugin from "@line/liff-inspector";
 import { useEffect } from "react";
 import { DeepDivePlugin } from "../plugin";
 
 export const useLiff = () => {
   useEffect(() => {
     liff.use(new DeepDivePlugin());
+    liff.use(new LIFFInspectorPlugin());
     liff.init({
       liffId: import.meta.env.VITE_LIFF_ID,
     });
